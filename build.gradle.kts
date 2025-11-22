90ef010962dfc89006591af4688ef19809dbd89a
linzhikuan <1065627939@qq.com>
2025-11-22 13:31:57 +0800

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+}
+
+// 应用通用库配置
+apply(from = "${rootProject.projectDir}/gradles/library.gradle")
+
+android {
+    namespace = "com.lzk.demo.core.socket"
+}
